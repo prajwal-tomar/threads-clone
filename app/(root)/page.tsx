dfde5a97d6ea7c5f97e96946bc87e26fed058ce4
Prajwal Tomar <prajwalstomar@gmail.com>
2023-08-22 20:59:37 +0530

    Added functionality to fetch all the threads from the DB and display on home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,37 @@
-export default function Home() {
-  return <div className="head-text">Threads</div>;
-}
+import ThreadCard from "@/components/cards/ThreadCard";
+import { fetchPosts } from "@/lib/actions/thread.actions";
+import { currentUser } from "@clerk/nextjs";
+import React from "react";
+
+const page = async () => {
+  const result = await fetchPosts(1, 30);
+  const user = await currentUser();
+  return (
+    <div>
+      <h1 className="head-text text-white">Home</h1>
+      <section className="mt-9 flex flex-col gap-10">
+        {result.posts.length === 0 ? (
+          <p className="no-result">No threads found</p>
+        ) : (
+          <>
+            {result.posts.map((post) => (
+              <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user?.id || ""}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
+    </div>
+  );
+};
+
+export default page;
